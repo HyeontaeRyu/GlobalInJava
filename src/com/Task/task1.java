5707e0ea2cfc687cbbeb409785e5e8b33247cd3c
Hyeontae Ryu <htRyu55@gmail.com>
2024-10-28 18:02:43 +0900

    dd

---
+++ src/com/Task/task1.java
@@ -0,0 +1,21 @@
+package com.Task;
+
+public class task1 {
+    public static void main(String[] args) {
+        int a = 1;
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println();
+            a += (i % 2 == 0) ? 4 : 0;
+            for (int j = 1; j <= 5; j++) {
+                printa(a);
+                a += (i % 2 == 0) ? -1 : 1;
+            }
+            a += (i % 2 == 0) ? 6 : 0;
+        }
+    }
+
+    public static void printa(int a) {
+        System.out.print(a + ((a < 10) ? "  " : " "));
+    }
+}

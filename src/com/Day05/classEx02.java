53da1027bc8334cf68429220fc144c513a6e6669
Hyeontae Ryu <htryu55@gmail.com>
2024-11-07 10:36:06 +0900

    dd

---
+++ src/com/Day05/classEx02.java
@@ -0,0 +1,18 @@
+package com.Day05;
+
+public class classEx02 {
+    private int pay;
+
+    public classEx02() {
+    }
+
+    public int getPay() {
+        return pay;
+    }
+
+    public void setPay(int pay) {
+        this.pay = pay;
+    }
+
+
+}
